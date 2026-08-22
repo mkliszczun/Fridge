8d0060dbb19a1eac6e7232b29fa271e027027a56
maciekkliszczun <maciekkliszczun@gmail.com>
2026-08-22 13:43:02 +0200

    Remove unused security classes

--- src/main/java/io/github/mkliszczun/fridge/security/UserDirectory.java
+++
@@ -1,7 +0,0 @@
-package io.github.mkliszczun.fridge.security;
-
-import java.util.UUID;
-//TODO - to delete soon
-public interface UserDirectory {
-//    UUID findUserIdByUsername(String username);
-}
--- src/main/java/io/github/mkliszczun/fridge/security/UserPrincipal.java
+++
@@ -1,6 +0,0 @@
-package io.github.mkliszczun.fridge.security;
-
-import java.util.UUID;
-
-public record UserPrincipal(UUID id, String username) {
-}
